b209cb882f1391270f43af06a501d2d02c3cfee7
Andrew Vreeland <122303931+AndrewVreeland@users.noreply.github.com>
2023-05-15 12:27:54 -0400

    Add files via upload

---
+++ QuantumSort/src/main/java/com/example/QuantumSort/QuantumSortApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuantumSort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumSortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantumSortApplication.class, args);
+	}
+
+}
---
+++ QuantumSort/src/test/java/com/example/QuantumSort/QuantumSortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuantumSort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumSortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
