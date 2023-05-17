e54f2d812c1bf4f24e3159c776e9e6d20cb395b2
Cameron Griffin (He/Him) <ct.griffin7@gmail.com>
2023-05-17 14:41:36 -0700

    added serach capability

--- QuantumSort/src/main/java/com/example/QuantumSort/repos/ListingRepository.java
+++ QuantumSort/src/main/java/com/example/QuantumSort/repos/ListingRepository.java
@@ -2,8 +2,15 @@ package com.example.QuantumSort.repos;
 
 import com.example.QuantumSort.models.Listing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ListingRepository extends JpaRepository<Listing, Long> {
 
-    Listing findById(String id);
+    @Query("SELECT l FROM Listing l WHERE l.id = ?1 OR l.city LIKE %?1%")
+    List<Listing> search(String query);
+
+    Optional<Listing> findById(Long id);
 }
